487ca5e1644a8d64ae3593e7c1604345c843b5f1
Adão Ribeiro <adao-eduardo@hotmail.com>
2023-06-06 22:21:28 -0300

    att tabelas

--- backend/src/Controllers/AreaControllers.ts
+++ backend/src/Controllers/AreaControllers.ts
@@ -1,21 +1,18 @@
 import { FastifyReply, FastifyRequest } from "fastify";
 
 
-
-
 export default new class AreaControllers {
     async Create(request: FastifyRequest, reply: FastifyReply) {
-
-
+      reply.send("ok")
     }
     async ListAll(request: FastifyRequest, reply: FastifyReply) {
-
+        reply.send("ok")
     }
     async Update(request: FastifyRequest, reply: FastifyReply) {
-
+        reply.send("ok")
     }
     async Delete(request: FastifyRequest, reply: FastifyReply) {
-
+        reply.send("ok")
     }
 }
 
